readjusted regularisation term (there was a mistake in the notebook formula)

notebooks/model.py
@@ -79,7 +79,7 @@ class RegressionModel:
         number_of_observations = len(self.y)
         predicted = self.X @ self.theta.T
         errors = predicted - self.y
-        regularisation = self.lambda_  * np.sum(self.theta[1:] ** 2)
+        regularisation = (self.lambda_/ number_of_observations)  * np.sum(self.theta[1:] ** 2)
         cost = (1 / (2 * number_of_observations)) * np.sum(errors ** 2) + regularisation
         return cost
 